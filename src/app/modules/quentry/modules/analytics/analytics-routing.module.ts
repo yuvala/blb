9711402b2afa6fd44b1d35458ff8040c44376f56
Avitan Barnes <avitan.barnes@brainlab.net>
2019-01-10 16:00:40 +0200

    lalal

--- src/app/modules/quentry/modules/analytics/analytics-routing.module.ts
+++ src/app/modules/quentry/modules/analytics/analytics-routing.module.ts
@@ -7,16 +7,18 @@ import { AnalyticsDashboardComponent } from './pages/dashboard/dashboard.compone
 import { AnalyticsChartsComponent } from './pages/charts/charts.component';
 
 const routes: Routes = [
+
     { path: 'analytics', component: AnalyticsMainComponent, children: [
         { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
         { path: 'dashboard', component: AnalyticsDashboardComponent },
         { path: 'charts/:id', component: AnalyticsChartsComponent },
         { path: '**', redirectTo: 'dashboard', pathMatch: 'full' }
+        
     ]},
 ];
 
 @NgModule({
-    
+
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 
