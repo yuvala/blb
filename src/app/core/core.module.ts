da77d27b452b2cb74108b7e703afbb81203799b1
Ruslanka <ruslank@ykm.co.il>
2019-01-10 21:21:45 +0200

    add logger

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,9 +1,10 @@
-import { NgModule } from '@angular/core';
+import { NgModule, ErrorHandler } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
 import { ApiService } from './services/api.service';
+import { LoggerService } from './services/logger.service';
 import { Page404Component } from './page-404/page404.component';
 
 @NgModule({
@@ -14,7 +15,8 @@ import { Page404Component } from './page-404/page404.component';
     ],
     providers: [
         //   { provide: HTTP_INTERCEPTORS, useClass: HttpTokenInterceptor, multi: true },
-        ApiService
+        ApiService,
+        { provide: ErrorHandler, useClass: LoggerService },
     ],
     declarations: [
         Page404Component
---
+++ src/app/core/services/logger.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { ErrorHandler } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+
+@Injectable()
+export class LoggerService extends ErrorHandler {
+    constructor() { super(); }
+    handleError(error) {
+        console.log(typeof error);
+        if (error instanceof HttpErrorResponse) {
+            // Backend returns unsuccessful response codes such as 404, 500 etc.
+            console.log('Backend returned status code: ', error.status);
+            console.log('Response body:', error.message);
+        } else {
+            // A client-side or network error occurred.
+            console.log('An error occurred:', error.message);
+        }
+        console.log(error);
+        console.log('***********************************\n\n\n');
+        // setTimeout(() => {
+        //     // console.clear();
+        //     console.log(error);
+        //     console.log(error.message);
+        //     console.log(error.stack);
+        // }, 2000);
+    }
+}
