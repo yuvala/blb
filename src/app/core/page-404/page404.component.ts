435526c5ca3fe74f79e0e2cf0d53f9b2c14ca2e3
Ruslan Katz <ruslan.katz@brainlab.net>
2019-01-10 13:46:58 +0200

    page404

--- src/app/core/page-404/page404.component.ts
+++ src/app/core/page-404/page404.component.ts
@@ -1,21 +1,16 @@
 import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute , Router  } from "@angular/router";
+import { ActivatedRoute, Router  } from "@angular/router";
 
 @Component({
     selector: 'app-page404',
     templateUrl: './page404.component.html',
-    styles: []
-
+    styleUrls: ['./page404.component.scss']
 })
 
 export class Page404Component implements OnInit {
     pathName: string;
   
     constructor(private route: ActivatedRoute, private router: Router) {}
-
-    sendMeHome() {
-        this.router.navigate(['']);
-      }
   
     ngOnInit() {
         this.pathName = "Login";
